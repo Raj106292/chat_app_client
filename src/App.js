1a650996090becb3f57adafaf1a349638d74ccfb
Raj106292 <rajdas.106292@gmail.com>
2023-02-25 21:56:52 +0600

    first launch in local server

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      
+    <div className='max-w-7xl mx-auto'>
+      <h1>hello</h1>
+      <button className="btn">Button</button>
     </div>
   );
 }
