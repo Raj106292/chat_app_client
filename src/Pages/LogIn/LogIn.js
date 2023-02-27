be55ff462463317cecbcb5f2bb30f7d6992e96b1
Raj106292 <rajdas.106292@gmail.com>
2023-02-27 19:36:52 +0600

    sign up page created

--- src/Pages/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './LogIn.css';
 
 const LogIn = () => {
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    // send an email to the user with password recovery link
     const handleForgetPassword = () => {
         console.log('hello');
+    }
+
+    // user log in to the app
+    const handleUserLogIn = (e) => {
+        e.preventDefault();
+
+        console.log(email, password);
     }
 
     return (
@@ -18,19 +30,24 @@ const LogIn = () => {
                         <label className="label">
                             <span className="label-text font-bold font-serif">Email</span>
                         </label>
-                        <input type="text" placeholder="Enter Your Email" className="bg-transparent custom-input" />
+                        <input type="email" placeholder="Enter Your Email" onChange={(e) => setEmail(e.target.value)} value={email} className="bg-transparent custom-input" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text font-bold font-serif">Password</span>
                         </label>
-                        <input type="text" placeholder="Enter Your Password" className="bg-transparent custom-input" />
+                        <input type="text" placeholder="Enter Your Password" onChange={(e) => setPassword(e.target.value)} value={password} className="bg-transparent custom-input" />
                         <label className="label flex justify-end">
                             <div className="label-text-alt link link-hover font-medium" onClick={handleForgetPassword}>Forgot password?</div>
                         </label>
+
+                        {/* navigate user to the sign up page */}
+                        <label className='label flex justify-start'>
+                            <div className='font-semibold text-purple-600'>Don't have an account? Please <Link to='/sign-up' className='text-blue-600 hover:underline'>Sign Up</Link></div>
+                        </label>
                     </div>
                     <div className="form-control mt-6">
-                        <button className="btn login-button bg-transparent"><span className='logIn'>Log In</span></button>
+                        <button onClick={handleUserLogIn} className="btn login-button bg-transparent"><span className='logIn'>Log In</span></button>
                     </div>
                 </div>
             </div>
--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
 import LogIn from "../Pages/LogIn/LogIn";
+import SignUp from "../Pages/SignUp/SignUp";
 
 export const routes = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ export const routes = createBrowserRouter([
             {
                 path:'/log-in',
                 element: <LogIn />
+            },
+            {
+                path:'/sign-up',
+                element: <SignUp />
             }
         ]
     }
